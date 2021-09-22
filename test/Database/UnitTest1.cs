8518db5b9778185e87446d96bc33ee614142ca5e
Alberto <Alberto@DESKTOP-IMM14BA>
2021-09-22 09:15:50 +0200

    Project moved to src folder. Test project moved to test folder.

---
+++ test/Database/UnitTest1.cs
@@ -0,0 +1,36 @@
+using MartianRobots.Database.Contexts;
+using MartianRobots.Database.Entities;
+using MartianRobots.Database.Repositories;
+using Moq;
+using System;
+using Xunit;
+
+namespace MartianRobots.UnitTest.Database
+{
+    public class Repositories
+    {
+        private StartUp _startUp = new StartUp();
+        private MartianRobotsRepository<Robot> _read;
+        private MartianRobotsRepository<Robot> _write;
+
+
+        [Fact]
+        public void Init()
+        {
+            _read = _startUp.GetInMemoryReadRepository();
+            _write = _startUp.GetInMemoryWriteRepository();
+        }
+
+        [Fact]
+        public void test1()
+        {
+            // Arrange
+            var mock = new Mock<Robot>();
+            mock.Setup(r => r.LastPosition = 1);
+
+            // Ac
+
+            // Assert
+        }
+    }
+}
---
+++ test/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using MartianRobots.Database.Contexts;
+using MartianRobots.Database.Entities;
+using MartianRobots.Database.Repositores.Base;
+using MartianRobots.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MartianRobots.UnitTest
+{
+    class StartUp
+    {
+        private readonly MartianRobotsContext _martianRobotsContext;
+
+        public StartUp()
+        {
+            var builder = new DbContextOptionsBuilder<MartianRobotsContext>();
+            builder.UseInMemoryDatabase(databaseName: "LibraryDbInMemory");
+
+            var dbContextOptions = builder.Options;
+            _martianRobotsContext = new MartianRobotsContext(dbContextOptions);
+            // Delete existing db before creating a new one
+            _martianRobotsContext.Database.EnsureDeleted();
+            _martianRobotsContext.Database.EnsureCreated();
+        }
+
+        public MartianRobotsRepository<Robot> GetInMemoryReadRepository()
+        {
+            return new MartianRobotsRepository<Robot>(_martianRobotsContext);
+        }
+
+        public MartianRobotsRepository<Robot> GetInMemoryWriteRepository()
+        {
+            return new MartianRobotsRepository<Robot>(_martianRobotsContext);
+        }
+
+    }
+}
